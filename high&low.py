d307904935171d99c0c2dbc32effbe48042bda08
khairunnisafitrisa <103508617+khairunnisafitrisa@users.noreply.github.com>
2022-04-19 23:56:33 +0800

    Add files via upload

---
+++ high&low.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+img_root='Images'
+img_name='nisaa.jpg'
+
+# Reading the Image
+img = cv2.imread(img_name,cv2.IMREAD_UNCHANGED)
+
+kernel = np.ones((10,10),np.float32)/25
+gaussBlur = cv2.GaussianBlur(img,(5,5),cv2.BORDER_DEFAULT)
+
+lowPass = cv2.filter2D(img,-1, kernel)
+lowPass = img - lowPass
+highPass = img - gaussBlur
+
+cv2.imshow("Low Pass",np.hstack((img, lowPass)))
+cv2.imshow("High Pass",np.hstack((img, highPass)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
